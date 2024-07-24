e62127905efebf209ca54a2d0fdc8d8048739cd1
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-07-25 00:09:33 +0530

    add 17. 13-roman-to-integer.ts

---
+++ 17. 13-roman-to-integer.ts
@@ -0,0 +1,22 @@
+function romanToInt(s: string): number {
+  const mapping: Record<string, number> = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    if ((i === s.length - 1) || mapping[s[i]] >= mapping[s[i + 1]]) {
+      result += mapping[s[i]];
+    } else {
+      result -= mapping[s[i]];
+    }
+  }
+  return result;
+};
+
+console.log(romanToInt("III"))
